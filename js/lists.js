1bd09915bb0c0a15f3178962c7bd80c56ddf89b4
Jason Burks <jason.burks@ge.com>
2014-08-27 17:47:39 -0400

    Traverse container tree looking for grocery lists
    The getUserLists() function will traverse the container tree for the
    logged in user and add all known grocery lists to the
    InventoryManager['lists'] array. Each list will be an object with the
    following properties:
    
      id: Firebase container id of the list
      name: The name of the grocery list (as specified at creation time)
      description: The grocery list description, if any

--- js/lists.js
+++ js/lists.js
@@ -21,3 +21,38 @@ function newList(data) {
     parentRef.update(update);
   }
 }
+
+// recursively descend through the container tree and identify all grocery lists
+// adding them to the context array (InventoryManger['lists']) and adding listeners
+// for their new children
+function recurseContainers(ref, obj) {
+  ref.once('value', function(v) {
+    if(v.val()['compType'] && v.val()['compType'] == "grocery") {
+      var listObj = {
+        id: ref.name(),
+        name: v.val()['name'],
+        description: v.val()['description']
+      };
+      obj.push(listObj);
+      var ch = ref.child('children');
+      ch.on('child_added', function(snap) {
+        recurseContainers(snap.ref().root().child('containers').child(snap.name()), this);
+      }, obj);
+    }
+  }, obj);
+}
+
+// starting at the logged-in user's rootContainer, enumerate all child
+// containers
+function getUserLists() {
+  if (!InventoryManager['uid']) {
+    flash('danger', 'You must be logged in to retrieve your containers.');
+    return false;
+  }
+  InventoryManager['lists'] = [];
+  var rootRef = InventoryManager['imRef'].child('containers').child(InventoryManager['rootContainer']).child('children');
+  rootRef.on('child_added', function(v) {
+    var contRef = InventoryManager['imRef'].child('containers').child(v.name());
+    recurseContainers(contRef, this);
+  }, InventoryManager['lists']);
+}
